4d09dc6622d9ab1186c1af9a746af1ea4e4c1665
강보영 <by28287@gmail.com>
2025-01-22 16:15:43 +0900

    feat: answer, comment, like 도메인 추가

---
+++ backend/src/main/java/com/ssafy/d3v/backend/answer/Answer.java
@@ -0,0 +1,46 @@
+package com.ssafy.d3v.backend.answer;
+
+import com.ssafy.d3v.backend.common.AccessLevel;
+import com.ssafy.d3v.backend.member.domain.Member;
+import com.ssafy.d3v.backend.question.domain.Question;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@ToString
+@Getter
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+public class Answer {
+    @Id
+    @Column(name = "answer_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int answerId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member memberId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "question_id", nullable = false)
+    private Question questionId;
+
+    @NotBlank
+    private String content;
+
+    @NotBlank
+    private LocalDateTime createdAt;
+
+    @NotBlank
+    private AccessLevel accessLevel;
+}
