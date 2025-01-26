241d5c7933e5500a0221930f21e5bf0fa4322e40
nahyewon <n417759@gmail.com>
2025-01-27 01:45:13 +0900

    #S12P11A609-85 refactor: navbar(sidebar) 삭제 및 header 영역 수정, 전역 text css 파일 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,5 @@
-import { useState } from 'react';
 import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-import Nav from './components/navbar/Nav.tsx';
 import Header from './components/header/Header.tsx';
 
 import HomePage from './pages/HomePage.tsx';
@@ -12,12 +10,9 @@ import MyPage from './pages/MyPage.tsx';
 import VideoPage from './pages/VideoPage.tsx';
 
 import './App.css';
+import './styles/TestStyle.css'
 
 function App() {
-  const [isNavOpen, setIsNavOpen] = useState<boolean>(true); // nav bar를 오픈헀는지 여부를 관리하는 useState()
-  const handleNavOpen = () => {
-    setIsNavOpen((prev) => !prev); // nav bar를 open/close 하는 함수
-  };
 
   // 레이아웃 라우트 정의
   const router = createBrowserRouter([
@@ -25,12 +20,9 @@ function App() {
       path: '/',
       element: (
         <div className="App">
-          <Header handleNavOpen={handleNavOpen} />
+          <Header />
           <div className="main-page">
-            <Nav isNavOpen={isNavOpen} />
-            <div className={`child-page ${!isNavOpen ? 'child-page--close' : ''}`}>
-              <Outlet />
-            </div>
+            <Outlet />
           </div>
         </div>
       ),
