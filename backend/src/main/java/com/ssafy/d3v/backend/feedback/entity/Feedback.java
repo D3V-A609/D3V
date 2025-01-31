0c01d1a5d0e7825a58c7720cddd21da9230cd204
강보영 <by28287@gmail.com>
2025-01-31 09:05:26 +0900

    S12P11A609-103 feat : feedback, follow 도메인 추가

---
+++ backend/src/main/java/com/ssafy/d3v/backend/feedback/entity/Feedback.java
@@ -0,0 +1,47 @@
+package com.ssafy.d3v.backend.feedback.entity;
+
+import com.ssafy.d3v.backend.answer.entity.Answer;
+import com.ssafy.d3v.backend.member.entity.Member;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@ToString
+@Getter
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+public class Feedback {
+    @Id
+    @Column(name = "feedback_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long feedbackId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "answer_id", nullable = false)
+    private Answer answerId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member memberId;
+
+    @NotBlank
+    private String content;
+
+    @NotBlank
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @NotBlank
+    @Column(name = "deleted_at")
+    private Boolean deletedAt;
+}
