a693730e6669647d9bb784a62941030024948030
강보영 <by28287@gmail.com>
2025-02-03 10:36:56 +0900

    refactor : swagger 태그 삭제

--- backend/src/main/java/com/ssafy/d3v/backend/like/controller/LikesController.java
+++ backend/src/main/java/com/ssafy/d3v/backend/like/controller/LikesController.java
@@ -26,24 +26,12 @@ public class LikesController {
     private final LikesService likesService;
 
     @PostMapping("/{answer_id}/like")
-    @Operation(summary = "좋아요", description = "답변에 대한 좋아요를 추가합니다.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "성공적으로 좋아요 추가",
-                    content = @Content(schema = @Schema(implementation = Question.class))),
-            @ApiResponse(responseCode = "404", description = "사용자나 질문을 찾을 수 없음")
-    })
     public ResponseEntity create(@PathVariable("answer_id") int answerId, LikesRequest request) {
         likesService.create(answerId, request);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @DeleteMapping("/{answer_id}/like")
-    @Operation(summary = "좋아요 취소", description = "좋아요를 취소합니다.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "성공적으로 좋아요 취소",
-                    content = @Content(schema = @Schema(implementation = Question.class))),
-            @ApiResponse(responseCode = "404", description = "좋아요를 찾을 수 없음")
-    })
     public ResponseEntity delete(@PathVariable("answer_id") int answerId) {
         likesService.delete(answerId);
         return ResponseEntity.ok().build();
