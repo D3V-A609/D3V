e533735299b9c9c60327b55f36f7156a1845608c
나혜원 <n417759@gmail.com>
2025-02-03 18:01:09 +0900

    #S12P11A609-107 feat: 베스트 답변 컴포넌트 구현

--- frontend/src/features/AnswerToggle/BestAnswer.tsx
+++ frontend/src/features/AnswerToggle/BestAnswer.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 
-const BestAnswer:React.FC = () => {
+import ThugUpImg from "../../assets/images/Thumbs-Up.png";
+
+interface BestAnswerProps {
+  bestAnswer: BestAnswer;
+}
+
+const BestAnswer:React.FC<BestAnswerProps> = ({bestAnswer}) => {
   return (
-  <div>
-    hi
-    <hr />
+  <div >
+    <div className="best-answer-container">
+          <div className="best-answer-title">
+            <img src={ThugUpImg} alt="thug-up-img" style={{width: '35px', height: '35px'}} />
+              Best 답변
+          </div>
+          <div className="best-answer-content">
+            <div className="answer-label">A.</div>
+            <div className="answer-text">{bestAnswer.standardAnswer}</div>
+          </div>
+        </div>
   </div>
 );
 }
