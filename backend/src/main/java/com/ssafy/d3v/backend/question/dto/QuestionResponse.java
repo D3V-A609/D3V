bde7a8beb482881b689f8ede8b1dc9a33fc85415
차정문 <ckwjdans4182@gmail.com>
2025-02-04 17:11:40 +0900

    S12P11A609-163 1차 MVP 전체 질문 API 임시 수정

--- backend/src/main/java/com/ssafy/d3v/backend/question/dto/QuestionResponse.java
+++ backend/src/main/java/com/ssafy/d3v/backend/question/dto/QuestionResponse.java
@@ -6,6 +6,7 @@ import com.ssafy.d3v.backend.question.entity.Question;
 import com.ssafy.d3v.backend.question.entity.Skill;
 import com.ssafy.d3v.backend.question.entity.SkillType;
 import java.util.List;
+import java.util.Random;
 import lombok.Builder;
 
 @Builder
@@ -13,14 +14,26 @@ public record QuestionResponse(
         Long questionId,
         String content,
         String standardAnswer,
+        String status,
+        Integer challengeCount,
+        Integer answerCount,
         List<SkillType> skillList,
         List<DevelopmentRole> jobList
 ) {
     public  static QuestionResponse from(Question question, List<Skill> skills, List<Job> jobs) {
+        // 임시 랜덤값 생성 코드
+        Random random = new Random();
+        String tempStatus = random.nextBoolean() ? "solved" : "unsolved";
+        int tempChallengeCount = random.nextInt(21); // 0부터 20까지
+        int tempAnswerCount = tempChallengeCount + random.nextInt((51 - tempChallengeCount));
+
         return QuestionResponse.builder()
                 .questionId(question.getId())
                 .content(question.getContent())
                 .standardAnswer(question.getStandardAnswer())
+                .status(tempStatus)
+                .challengeCount(tempChallengeCount)
+                .answerCount(tempAnswerCount)
                 .skillList(skills.stream().map(Skill::getName).toList())
                 .jobList(jobs.stream().map(Job::getDevelopmentRole).toList())
                 .build();
