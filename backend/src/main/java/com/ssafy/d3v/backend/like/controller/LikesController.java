c82f735698a077787473d9d40a779ad4452e40ca
강보영 <by28287@gmail.com>
2025-02-05 08:32:08 +0900

    hotifx : 답변, 좋아요 controller @RequestBody 추가

--- backend/src/main/java/com/ssafy/d3v/backend/like/controller/LikesController.java
+++ backend/src/main/java/com/ssafy/d3v/backend/like/controller/LikesController.java
@@ -15,6 +15,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,7 +27,7 @@ public class LikesController {
     private final LikesService likesService;
 
     @PostMapping("/{answer_id}/like")
-    public ResponseEntity create(@PathVariable("answer_id") int answerId, LikesRequest request) {
+    public ResponseEntity create(@PathVariable("answer_id") int answerId, @RequestBody LikesRequest request) {
         likesService.create(answerId, request);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
