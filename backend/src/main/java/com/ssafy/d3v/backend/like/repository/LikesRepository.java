f48567aabd105ab6998a0d44d92d68729d66a236
강보영 <by28287@gmail.com>
2025-02-06 10:20:29 +0900

    fix : 답변 id에 따른 좋아요 수 조회

--- backend/src/main/java/com/ssafy/d3v/backend/like/repository/LikesRepository.java
+++ backend/src/main/java/com/ssafy/d3v/backend/like/repository/LikesRepository.java
@@ -10,4 +10,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface LikesRepository extends JpaRepository<Likes, Long> {
     Optional<Likes> findByAnswerAndMember(Answer answerId, Member memberId);
+    int countByAnswer(Answer answer);
 }
