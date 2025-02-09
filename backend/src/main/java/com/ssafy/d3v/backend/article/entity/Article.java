b6f9389589587418042d19608a4e3019774ed266
강보영 <by28287@gmail.com>
2025-02-09 17:26:56 +0900

    feat : articleImage 도메인 추가

--- backend/src/main/java/com/ssafy/d3v/backend/article/entity/Article.java
+++ backend/src/main/java/com/ssafy/d3v/backend/article/entity/Article.java
@@ -2,6 +2,7 @@ package com.ssafy.d3v.backend.article.entity;
 
 import com.ssafy.d3v.backend.common.BaseEntity;
 import com.ssafy.d3v.backend.member.entity.Member;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -10,7 +11,9 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -20,7 +23,7 @@ import lombok.ToString;
 @Entity
 @ToString
 @Getter
-@Builder
+@Builder(toBuilder = true)
 @AllArgsConstructor
 @NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
 public class Article extends BaseEntity {
@@ -41,5 +44,18 @@ public class Article extends BaseEntity {
 
     private String content;
 
+    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Builder.Default
+    private List<ArticleImage> imageUrls = new ArrayList<>();
+
     private Integer view;
+
+    @Column(name = "comment_count")
+    private Integer commentCount;
+
+    public void addImage(ArticleImage image) {
+        imageUrls.add(image);
+        image.setArticle(this);
+    }
+
 }
