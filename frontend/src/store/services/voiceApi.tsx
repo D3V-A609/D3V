32a27b483ebc0b57ed5e4d630b894b2e961ef946
나혜원 <n417759@gmail.com>
2025-02-11 09:40:00 +0900

    #S12P11A609-87 refact: url 변경

--- frontend/src/store/services/voiceApi.tsx
+++ frontend/src/store/services/voiceApi.tsx
@@ -3,7 +3,7 @@ import api from './api';
 export const voiceApi = {
 
   sendVoiceRecording: async (formData: FormData) => {
-    const response = await api.post('/voice', formData, {
+    const response = await api.post('/speech/text', formData, {
       headers: {
         'Content-Type' : 'multipart/form-data',
       },
