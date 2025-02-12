b2d94dcbc22760ffdf6ff20f2be8c4e4cd031f0b
강보영 <by28287@gmail.com>
2025-02-12 10:41:58 +0900

    fix : 허용 url 변경

--- backend/src/main/java/com/ssafy/d3v/backend/common/config/CorsConfig.java
+++ backend/src/main/java/com/ssafy/d3v/backend/common/config/CorsConfig.java
@@ -1,11 +1,11 @@
 package com.ssafy.d3v.backend.common.config;
 
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-import java.util.List;
 
 @Configuration
 public class CorsConfig {
@@ -14,13 +14,13 @@ public class CorsConfig {
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
-        
+
         config.setAllowedOrigins(List.of("http://d3vtest.s3-website.ap-northeast-2.amazonaws.com"));  // ✅ 허용할 Origin 설정
         config.setAllowedMethods(List.of("GET", "POST", "OPTIONS", "PUT", "DELETE"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         config.setAllowCredentials(true);  // ✅ withCredentials 지원
-        
-        source.registerCorsConfiguration("/api/**", config);
+
+        source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
     }
 }
