be877ee4513f4465adfd80e82e48bb046d2dc1ca
나혜원 <n417759@gmail.com>
2025-02-12 14:28:43 +0900

    #S12P11A609-280 refact: 에러 핸들링 로직 추가

--- frontend/src/store/services/voiceApi.tsx
+++ frontend/src/store/services/voiceApi.tsx
@@ -3,11 +3,16 @@ import api from './api';
 export const voiceApi = {
 
   sendVoiceRecording: async (formData: FormData) => {
-    const response = await api.post('/speech/text', formData, {
-      headers: {
-        'Content-Type' : 'multipart/form-data',
-      },
-    });
-    return response.data;
+    try{
+      const response = await api.post('/speech/text', formData, {
+        headers: {
+          'Content-Type' : 'multipart/form-data',
+        },
+      });
+      return response.data;
+    }catch(error){
+      console.log('in voice api error: ', error);
+      throw new Error('음성을 변환하는데 문제가 발생했습니다.')
+    }
   },
 }
