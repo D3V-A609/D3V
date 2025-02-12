2cddb417b9a81c7dd1f5249be442d8a2a19502bc
cupwan <pkwmj44@gmail.com>
2025-02-12 14:52:39 +0900

    fix: cors 에러, 환경변수 설정

--- backend/src/main/java/com/ssafy/d3v/backend/common/config/CorsConfig.java
+++ backend/src/main/java/com/ssafy/d3v/backend/common/config/CorsConfig.java
@@ -1,18 +1,16 @@
 package com.ssafy.d3v.backend.common.config;
 
-//@Configuration
-//public class CorsConfig {
-//
-//    @Bean
-//    public CorsConfigurationSource corsConfigurationSource() {
-//        CorsConfiguration configuration = new CorsConfiguration();
-//        configuration.setAllowedOriginPatterns(List.of("http://d3vtest.s3-website.ap-northeast-2.amazonaws.com"));
-//        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-//        configuration.setAllowedHeaders(List.of("*"));
-//        configuration.setAllowCredentials(true);
-//
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", configuration);
-//        return source;
-//    }
-//}
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://d3vtest.s3-website.ap-northeast-2.amazonaws.com/")  // 허용할 출처 명시
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true);
+    }
+}
