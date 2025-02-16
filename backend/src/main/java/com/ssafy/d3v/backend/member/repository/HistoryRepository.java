aebce39eb2151e5c098bef62562105a3a5927a21
강보영 <by28287@gmail.com>
2025-02-16 17:28:45 +0900

    fix : history 데이터 바탕으로 maxstreak, ongoingstreak 변영 스케줄러 추가

--- backend/src/main/java/com/ssafy/d3v/backend/member/repository/HistoryRepository.java
+++ backend/src/main/java/com/ssafy/d3v/backend/member/repository/HistoryRepository.java
@@ -1,10 +1,16 @@
 package com.ssafy.d3v.backend.member.repository;
 
 import com.ssafy.d3v.backend.member.entity.History;
+import com.ssafy.d3v.backend.member.entity.Member;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HistoryRepository extends JpaRepository<History, Long> {
     List<History> findByMemberIdAndDateBetween(long memberId, LocalDate start, LocalDate end);
+
+    List<History> findByMemberOrderByDateAsc(Member member);
+
+    Optional<History> findByMemberIdAndDate(long memberId, LocalDate date);
 }
