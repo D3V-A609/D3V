a41328844746691d5d421bd8a654d6863d2f5fe9
yujin <erin90523@naver.com>
2025-02-17 02:52:57 +0900

    S12P11A609-334 제작중

---
+++ frontend/src/features/Video/VideoDetail.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './VideoDetail.css';
+
+interface VideoDetailProps {
+  videoId: string;
+  onBack: () => void;
+}
+
+const VideoDetail: React.FC<VideoDetailProps> = ({ videoId, onBack }) => (
+  <div className="video-detail">
+    <button onClick={onBack} className="back-button">뒤로 가기</button>
+    <div className="video-wrapper">
+      <iframe
+        width="560"
+        height="315"
+        src={`https://www.youtube.com/embed/${videoId}`}
+        frameBorder="0"
+        allow="autoplay; encrypted-media"
+        allowFullScreen
+      ></iframe>
+    </div>
+  </div>
+);
+
+export default VideoDetail;
