127d87e449b5eb04a5500ecf686806a425da5090
yujin <erin90523@naver.com>
2025-02-17 11:44:06 +0900

    #S12P11A609-334 feat: 면접 영상 추천 페이지 구현

--- frontend/src/pages/VideoPage.tsx
+++ frontend/src/pages/VideoPage.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { useYoutubeApi } from '../store/services/YoutubeApi';
-import SearchBar from '../components/SearchBar/SearchBar';
 import VideoList from '../features/Video/VideoList';
 import VideoDetail from '../features/Video/VideoDetail';
 import PageHeader from "../components/PageHeader/PageHeader"
@@ -8,19 +7,13 @@ import { FaYoutube } from "react-icons/fa6";
 import './VideoPage.css';
 
 const VideoPage: React.FC = () => {
-  const { videos, loading, error, fetchVideos, nextPageToken } = useYoutubeApi();
+  const { videos, error, fetchVideos, nextPageToken } = useYoutubeApi();
   const [selectedVideoId, setSelectedVideoId] = useState<string | null>(null);
-  const [searchQuery, setSearchQuery] = useState('');
+  const [searchQuery] = useState('');
 
   useEffect(() => {
     fetchVideos('software developer interview');
   }, [fetchVideos]);
-
-  const handleSearch = (query: string) => {
-    setSearchQuery(query);
-    fetchVideos(query);
-    setSelectedVideoId(null);
-  };
 
   const handleLoadMore = () => {
     if (nextPageToken) {
@@ -36,24 +29,20 @@ const VideoPage: React.FC = () => {
       icon={<FaYoutube />}
       iconStyle="youtube-icon"
     />
-    <div className="search-container">
-        <SearchBar onSearch={handleSearch} searchQuery={searchQuery} />
-      </div>
-      {loading && <p className="loading-message">Loading...</p>}
-      {error && <p className="error-message">Error: {error}</p>}
-      {selectedVideoId ? (
-        <VideoDetail videoId={selectedVideoId} onBack={() => setSelectedVideoId(null)} />
-      ) : (
-        <>
-          <VideoList videos={videos} onVideoSelect={setSelectedVideoId} />
-          {nextPageToken && (
-            <button onClick={handleLoadMore} className="load-more-button">
-              더 보기
-            </button>
-          )}
-        </>
-      )}
-    </div>
+    {error && <p className="error-message">Error: {error}</p>}
+    {selectedVideoId ? (
+      <VideoDetail videoId={selectedVideoId} onBack={() => setSelectedVideoId(null)} />
+    ) : (
+      <>
+        <VideoList videos={videos} onVideoSelect={setSelectedVideoId} />
+        {nextPageToken && (
+          <button onClick={handleLoadMore} className="load-more-button">
+            더 보기
+          </button>
+        )}
+      </>
+    )}
+  </div>
   );
 };
 
