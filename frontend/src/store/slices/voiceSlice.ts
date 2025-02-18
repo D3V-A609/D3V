f5036f9b57b778c35d99c4f791adeeeabb174180
나혜원 <n417759@gmail.com>
2025-02-18 15:24:18 +0900

    #S12P11A609-361 fix: 음성 데이터 전달 형식 변경

--- frontend/src/store/slices/voiceSlice.ts
+++ frontend/src/store/slices/voiceSlice.ts
@@ -29,7 +29,10 @@ const voiceSlice = createSlice({
       .addCase(sendVoiceRecording.fulfilled, (state, action) => {
         state.uploading = false;
         state.uploadSuccess = true;
-        state.speechToText = action.payload;  // 서버 응답 값 저장
+        // console.log("결과: ", action.payload)
+        state.speechToText = action.payload.text;  // 서버 응답 값 저장
+        if(action.payload.text === "" ) alert('음성 인식이 불안정해 변환이 어렵습니다.. 또렷한 목소리로 다시 한번 답변해주세요. 죄송합니다.')
+
       })
       .addCase(sendVoiceRecording.rejected, (state, action) => {
         state.uploading = false;
