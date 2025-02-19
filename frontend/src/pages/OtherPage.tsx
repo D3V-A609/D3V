5c8b6d389423c38f8f66ff253af87d74ebabb1ea
나혜원 <n417759@gmail.com>
2025-02-19 09:02:29 +0900

    #S12P11A609-318 fix: other page 수정

--- frontend/src/pages/OtherPage.tsx
+++ frontend/src/pages/OtherPage.tsx
@@ -7,17 +7,13 @@ import FollowModalView from '../components/MyPage/FollowModalView';
 import StreakHeatMap from '../features/My/StreakHeatMap/StreakHeatMap';
 import { shallowEqual } from 'react-redux';
 import { fetchUserFollowers, fetchUserFollowings, fetchUserInfo, unFollow } from '../store/actions/userActions';
-import { BsPostcardHeart } from 'react-icons/bs';
 import { FaBookmark, FaBook } from 'react-icons/fa';
 import { FcVoicePresentation } from 'react-icons/fc';
-import { GoComment } from 'react-icons/go';
 import { IoCheckboxOutline } from 'react-icons/io5';
 import ContentPreviewList from '../components/MyPage/ContentPreviewList';
 import SectionContainer from '../components/MyPage/SectionContainer';
 import UserInfoComp from '../features/My/UserInfoComp';
 import { QuestionState } from '../store/slices/questionSlice';
-import { ArticleState } from '../store/slices/articleSlice';
-import { fetchMyArticles, fetchMyArticleComments } from '../store/actions/articleActions';
 import { fetchMyLastedQuestions } from '../store/actions/questionActions';
 import { UserState } from '../store/slices/userSlice';
 
@@ -39,7 +35,6 @@ const OtherPage: React.FC = () => {
 
   const { other } = useAppSelector((state) => state.user as UserState, shallowEqual)
   const { MySolvedQuestions } = useAppSelector((state) => state.questions as QuestionState, shallowEqual)
-  const { myArticles, myArticleComments } = useAppSelector((state) => state.articles as ArticleState, shallowEqual)
 
   // API 중복 호출 방지
   const hasFetched = useRef(false);
@@ -50,10 +45,6 @@ const OtherPage: React.FC = () => {
       Promise.all([
         // 사용자가 푼 답변 로드
         dispatch(fetchMyLastedQuestions(true)), // 바꿔야 함
-        
-        // 사용자가 작성한 게시글/댓글
-        dispatch(fetchMyArticles(Number(memberId))),
-        dispatch(fetchMyArticleComments(Number(memberId))),
 
         // 사용자 정보 불러오기
         dispatch(fetchUserInfo(Number(memberId))),
@@ -118,8 +109,6 @@ const OtherPage: React.FC = () => {
       BookIcon: <FaBook size={24} color='#8B4513' />,
       CommuIcon: <FcVoicePresentation size={28} />,
       checkbox: <IoCheckboxOutline size={28} color='#40C463' />,
-      post: <BsPostcardHeart size={28} color='#40C463' />,
-      post_answer: <GoComment size={28} color='#40C463' />
   };
 
   const UserInfoCompMemo = React.memo(UserInfoComp);
@@ -136,24 +125,12 @@ const OtherPage: React.FC = () => {
 
         <div className='my-answer-info-container'>
             <ContentPreviewListMemo contents={MySolvedQuestions} title='내가 답변한 질문' titleIcon={icons.checkbox} className='my-question-info' handleDetail='answer-detail' handleMoreBtn={() => openModal('내 답변', icons.checkbox, MySolvedQuestions, 'answer')}/>
-            {/* <ContentPreviewListMemo contents={MyUnsolvedQuestions} title='답변하지 못한 질문' titleIcon={icons.xbox} className='my-question-info' handleDetail='answer-detail' handleMoreBtn={() => moveToAllQuesitons(navigate, false)}/> */}
         </div>
         
         <SectionContainerMemo className='my-learning-info-container' title='학습 활동' icon={icons.BookIcon}>
             <div className="my-streak">
                 <StreakHeatMap />
             </div>
-            {/* <div className='my-answer-commu-activity'>
-                <div className='my-answer-info-container'>
-                    <ContentPreviewListMemo contents={likedAnswers} title='추천한 답변' titleIcon={icons.thumbup} className='my-question-info' handleDetail='answer-commu' handleMoreBtn={() => openModal('추천한 답변', icons.thumbup, likedAnswers, 'answer-commu')}/>
-                    <ContentPreviewListMemo contents={myFeedbacks} title='나의 피드백' titleIcon={icons.goComment} className='my-question-info'handleDetail='answer-commu' handleMoreBtn={() => openModal('나의 피드백', icons.goComment, myFeedbacks, 'answer-commu')} />
-                </div>
-            </div> */}
-        </SectionContainerMemo>
-
-        <SectionContainerMemo className='my-commu-info-container' title='커뮤니티 활동' icon={icons.CommuIcon} >
-            <ContentPreviewListMemo contents={myArticles} title='내 커뮤니티' titleIcon={icons.post} className='my-question-info' handleDetail='article' handleMoreBtn={() => openModal('내 커뮤니티', icons.post, myArticles, 'article')} />
-            <ContentPreviewListMemo contents={myArticleComments} title='내 댓글' titleIcon={icons.post_answer} className='my-question-info' handleDetail='article' handleMoreBtn={() => openModal('내 댓글', icons.post_answer, myArticleComments, 'acticle')} />
         </SectionContainerMemo>
 
         {/* 모달 렌더링 */}
