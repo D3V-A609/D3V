5bd1b24c86568ed47232894e45a45716df47a391
나혜원 <n417759@gmail.com>
2025-02-19 10:50:22 +0900

    #S12P11A609-380 fix: header의 사진 이미지로 변경

--- frontend/src/components/header/UserProfileImg.tsx
+++ frontend/src/components/header/UserProfileImg.tsx
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../../store/hooks/useRedux";
+import { fetchUserInfo } from "../../store/actions/userActions";
+import { UserState } from "../../store/slices/userSlice";
 
 interface UserInfoProps {
   className?: string;  // className을 선택적으로 받을 수 있도록 설정
 }
 
 const UserProfileImg: React.FC<UserInfoProps> = ({className}) => {
+
+  const dispatch = useAppDispatch();
+  const { me } = useAppSelector((state) => state.user as UserState)
+
+  useEffect(()=>{
+    dispatch(fetchUserInfo(null))
+  }, [])
+
+  // const {}
   return (
-      <div className={`header-container_user-avatar ${className || ""}`}>D3V</div>
+      <div className={`header-container_user-avatar ${className || ""}`}>
+        <div className="profile-avatar">
+          {me?.profileImg ? (
+            <img src={me.profileImg} alt="프로필" />
+          ) : (
+            <div className="avatar-fallback">{me?.nickname[0].toUpperCase()}</div>
+          )}
+        </div>
+    </div>
   );
 };
 
